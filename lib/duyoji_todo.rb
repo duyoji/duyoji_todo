89893aeb27851408d8a5aa3f1bee6b7383c9163a
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2013-12-01 03:40:35 +0100

    15-4-2完了

--- lib/duyoji_todo.rb
+++ lib/duyoji_todo.rb
@@ -1,5 +1,6 @@
 require "duyoji_todo/command"
 require "duyoji_todo/db"
+require "duyoji_todo/task"
 require "duyoji_todo/version"
 
 module DuyojiTodo
---
+++ lib/duyoji_todo/task.rb
@@ -0,0 +1,24 @@
+#coding: utf-8
+
+require 'active_record'
+
+module DuyojiTodo
+
+	# tasksテーブルを表現するモデルクラスです
+	# @author Tsuyoshi Maeda 
+	class Task < ActiveRecord::Base
+
+		NOT_YET = 0 # タスクが完了していない
+		DONE    = 1 # タスクが完了した
+		PENDING = 2 # 保留状態
+		
+    # ステータ薄の名称と数値の組み合わせの値
+		STATUS = {
+		  'NOT_YET' => NOT_YET,
+		  'DONE'    => DONE,
+      'PENDING' => PENDING
+		}.freeze
+
+	end
+
+end
